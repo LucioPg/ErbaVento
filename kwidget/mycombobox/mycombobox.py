81e9ee2c1e21b3d7f02b2ea7b95496327da552bd
lucio <lucio.di.capua@gmail.com>
2019-10-17 03:40:52 +0200

    custom combobox per aggiungere platforms con il tasto destro

--- kwidget/mycombobox/mycombobox.py
+++ kwidget/mycombobox/mycombobox.py
@@ -1,22 +1,82 @@
 import sys
 
-from PyQt5.QtCore import QEvent
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QWidget
 
 
 class MyWidget(QComboBox):
-
+    EDITFINISHED = pyqtSignal(str)
     def __init__(self, parent=None):
         super(MyWidget, self).__init__(parent)
-        self.installEventFilter(self)
+        self.listaPlatform = ['Booking', 'AirB&B', 'Privati']
+        self.preferPlat = 'Booking'
+        # self.preferPlat = 'Privati'
+        for p in self.listaPlatform:
+            self.addPlatform(p)
+        self.sortPreferPlat()
+        # self.installEventFilter(self)
 
-    def eventFilter(self, QObject, event):
-        if event.type() == QEvent.MouseButtonPress:
-            if event.button() == Qt.RightButton:
-                print("Right button clicked")
-        return super(MyWidget, self).eventFilter(QObject, event)
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.on_context_menu)
+
+        # create context menu
+        self.popMenu = QMenu(self)
+        aggiungiAction = QAction('Aggiungi', self)
+        self.popMenu.addAction(aggiungiAction)
+        self.popMenu.addSeparator()
+        rimuoviAction = QAction('Rimuovi', self)
+        self.popMenu.addAction(rimuoviAction)
+        self.popMenu.addSeparator()
+        preferitoAction = QAction('Preferito', self)
+        self.popMenu.addAction(preferitoAction)
+        self.setEditable(False)
+        aggiungiAction.triggered.connect(self.scriviNuovaPlat)
+        rimuoviAction.triggered.connect(self.rimuoviPlat)
+        preferitoAction.triggered.connect(self.setPreferrPlat)
+
+    @pyqtSlot()
+    def addPlatform(self, plat=None):
+        if plat is None:
+            plat = self.currentText()
+        if plat not in self.listaPlatform:
+            self.listaPlatform.append(plat)
+        if -1 == self.findText(plat, Qt.MatchExactly):
+            self.addItem(plat)
+            self.model().sort(1)
+        else:
+            self.setEditable(False)
+            return
+        self.setEditable(False)
+
+    def editFinished(self):
+        text = self.lineEdit().text()
+        return text
+
+    def on_context_menu(self, point):
+        # show context menu
+        self.popMenu.exec_(self.mapToGlobal(point))
+
+    def rimuoviPlat(self):
+        ind = self.currentIndex()
+        text = self.currentText()
+        self.removeItem(ind)
+        print(text, ' rimosso')
+
+    def scriviNuovaPlat(self):
+        self.setEditable(True)
+        # QLineEdit.editingFinished()
+        self.lineEdit().editingFinished.connect(self.addPlatform)
+
+    def setPreferrPlat(self):
+        text = self.currentText()
+        self.preferPlat = text
+        print("preferito: ", self.preferPlat)
+
+    def sortPreferPlat(self):
+        item = self.findText(self.preferPlat, Qt.MatchExactly)
+        self.setCurrentIndex(item)
 
 
 if __name__ == "__main__":
