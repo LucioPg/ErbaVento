5c5d7ef4b38f77fdcaaff7b29c3715592bc07220
lucio <lucio.di.capua@gmail.com>
2019-10-29 20:50:16 +0100

    aggiunta modalità display importi sulla base dei numeri dei letti, della stagione e della piattaforma

--- kwidget/dialog_opt/dialog_opt.py
+++ kwidget/dialog_opt/dialog_opt.py
@@ -14,32 +14,101 @@ class DialogOption(DialogOptionGui, QtWidgets.QDialog):
                                  'AirB&B',
                                  'Privati'
                                  ]
-        self.defaultImporti = {'Booking.com': [72, 74, 92, 111, 130],
+        self.defaultImporti = {'Booking': [72, 74, 92, 111, 130],
                                'AirB&B': [64, 65, 85, 100, 117],
                                'Privati': [72, 74, 92, 111, 130]}
-        self.defaultProvvigioni = [0.15, 0.03, 0]
+        self.defaultProvvigioni = {'Booking.com': 0.15, 'AirB&B': 0.03, 'Privati': 0}
         self.config = {}
         self.defaultConfig = {'numero letti': 5,
                               'platforms': self.defaultPlatforms,
-                              'stagione': {'alta': {'provvigioni': {pl: prov for pl, prov in zip(self.defaultPlatforms,
-                                                                                                 self.defaultProvvigioni)},
+                              'stagione': {
+                                  'Alta': {'provvigioni': {pl: prov for pl, prov in self.defaultProvvigioni.items()},
                                                     'importi': self.defaultImporti
-                                                    },
-                                           'bassa': {'provvigioni': {pl: prov for pl, prov in zip(self.defaultPlatforms,
-                                                                                                  self.defaultProvvigioni)},
-                                                     'importi': self.defaultImporti
-                                                     },
-                                           'media': {'provvigioni': {pl: prov for pl, prov in zip(self.defaultPlatforms,
-                                                                                                  self.defaultProvvigioni)},
-                                                     'importi': self.defaultImporti
-                                                     }
                                            },
+                                  'Bassa': {'provvigioni': {pl: prov for pl, prov in self.defaultProvvigioni.items()},
+                                                     'importi': self.defaultImporti
+                                            },
+                                  'Media': {'provvigioni': {pl: prov for pl, prov in self.defaultProvvigioni.items()},
+                                                     'importi': self.defaultImporti
+                                            }
+                                  },
                               }
-
         self.checkConfigFile()
+        self.loadConfig()
+        # funzioni pulsanti
+        self.bot_aggiungiLetto.clicked.connect(self.aggiungiLetto)
+        self.bot_sottraiLetto.clicked.connect(self.sottraiLetto)
+
+        # funzioni comboBoxes
+        self.combo_platform.currentTextChanged.connect(self.displayImporti)
+        self.combo_stagione.currentIndexChanged.connect(self.displayImporti)
+
+    def aggiungiLetto(self):
+        try:
+            numeroAttuale = int(self.label_numeroLetti.text())
+            numero = str(numeroAttuale + 1)
+            self.setNumeroLetti(numero)
+            self.config['numero letti'] = numero
+            self.displayImporti()
+        except:
+            print(fex())
+
+    def sottraiLetto(self):
+        try:
+            numeroAttuale = int(self.label_numeroLetti.text())
+            if numeroAttuale == 0:
+                return
+            numero = str(numeroAttuale - 1)
+            self.setNumeroLetti(numero)
+            self.config['numero letti'] = numero
+            if int(numero) > 0:
+                self.tableWidget__importi.setRowCount(int(numero))
+        except:
+            print(fex())
+
+    def setNumeroLetti(self, nl):
+        self.label_numeroLetti.setText(nl)
+
+    def setProvvigioni(self, plat, prov):
+        self.defaultProvvigioni[plat] = prov
+
+    def displayImporti(self):
+        try:
+            rows = int(self.label_numeroLetti.text())
+            self.tableWidget__importi.setRowCount(rows)
+            stagione = self.combo_stagione.currentText()
+            platform = self.combo_platform.currentText()
+            flagNuvoImporto = False
+            for i in range(rows):
+                itemNumero = QtWidgets.QTableWidgetItem()
+                itemImporto = QtWidgets.QTableWidgetItem()
+                itemNumero.setText(str(i + 1))
+                try:
+                    importo = self.config['stagione'][stagione]['importi'][platform][i]
+                    itemImporto.setText(str(importo))
+                except IndexError:
+                    flagNuvoImporto = True
+
+                self.tableWidget__importi.setItem(i, 0, itemNumero)
+                self.tableWidget__importi.setItem(i, 1, itemImporto)
+                if flagNuvoImporto:
+                    self.setNuovoImporto()
+        except:
+            print(fex())
+
+    def setNuovoImporto(self):
+        row = self.tableWidget__importi.rowCount()
+        self.tableWidget__importi.setFocus(True)
+        self.tableWidget__importi.scrollToBottom()
+        item = self.tableWidget__importi.item(row - 1, 1)
+        item.setSelected(True)
+        self.tableWidget__importi.editItem(item)
 
     def loadConfig(self):
-        pass
+        if self.checkConfigFile():
+            self.setNumeroLetti(str(self.config['numero letti']))
+            self.displayImporti()
+
 
     def saveConfig(self):
         with open(self.fileConf, 'w') as fileconfig:
@@ -53,7 +122,8 @@ class DialogOption(DialogOptionGui, QtWidgets.QDialog):
         except FileNotFoundError:
             self.config = deepcopy(self.defaultConfig)
             self.saveConfig()
-            return print('File di configurazione creato')
+            print('File di configurazione creato')
+        return True
 
 
 if __name__ == "__main__":
