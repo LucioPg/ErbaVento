d144dd5a43158f96f44b58195ee4f0556d5c8554
Lucio <lucio.di.capua@gmail.com>
2019-12-02 23:58:31 +0100

    spese aren't setted for all days in the month anymore
    trouble for to show "spese finora" in infoStat

--- leggistoDb.py
+++ leggistoDb.py
@@ -2,11 +2,12 @@ import pickle
 
 _file = 'database.pkl'
 
+# with open(_file, 'rb') as f:
+#     dati = f.readlines()
 with open(_file, 'rb') as f:
-    dati = f.readlines()
-
+    dati = pickle.load(f)
 a = 2019
-m = 10
+m = 12
 g = 2
 for x, y in dati[a][m][g]['checkIn'].items():
     print(x, ' ', y)
